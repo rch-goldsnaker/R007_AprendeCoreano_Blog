0114bdf32870441476009e04f8eb983faaf6e0a8
rch-goldsnaker <rch.goldsnaker@gmail.com>
2022-04-16 22:19:28 -0300

    Fixing erors from es lint

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -4,7 +4,7 @@ import moment from 'moment';
 
 const PostDetail = ({ post }) => {
   return (
-    <React.Fragment>
+    <>
       <div className="bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8">
         <div className="relative overflow-hidden shadow-md mb-6">
           <img src={post.featuredImage.url} alt="" className="object-top h-full w-full object-cover  shadow-lg rounded-t-lg lg:rounded-lg" />
@@ -32,7 +32,7 @@ const PostDetail = ({ post }) => {
           {parse(post.content.html)}
         </div>
       </div>
-    </React.Fragment>
+    </>
   );
 };
 
